5c47dea97ae80f5033fabc4bb608b214c7be887c
Nic Last <niclast7611@gmail.com>
2022-05-13 16:20:47 -0700

    completed coding concepts, need to finsih one question on code challenges, havent touched interview

--- code-challenges.js
+++ code-challenges.js
@@ -21,21 +21,85 @@ const temp1 = 42
 const temp2 = 350
 const temp3 = 212
 
+//Create a function named boilingPoint 
+    //It will take a number argument named temp 
+        //this will allow us to compare this number to boiling point determining if it is below above or at boiling point 
+    // we need to create a conditional allowing the computer to make decisions hense forth a decision tree 
+        //if the temp > 212F 
+            //return `${temp} is above boiling point!`
+        //else if allowing us to give another condition. is the temp === 212F
+            // return `${temp} is at boiling point!`
+        // else giving us a catch all for all other numbers lower than 212
+            //return `${temp} is below boiling point!`
+        //Call the function boilingPoint() in a console.log
+    
+    const boilingPoint = (temp) => {
+        if(temp > 212){
+            return `${temp} is above boiling point!`
+        } else if (temp === 212){
+            return `${temp} is at boiling point!`
+        } else {
+            return `${temp} is below boiling point!`
+        }
+    }
+            console.log(boilingPoint(temp1))
+            console.log(boilingPoint(temp2))
+            console.log(boilingPoint(temp3))
+
+                //Output ---> 42 is below boiling point
+                //Output ---> 350 is above boiling point
+                //Output ---> 212 is at boiling point 
+                //Also tried with several other numbers all were represented with expected outputs 
+
 
 // --------------------2) Create the code that will combine the two arrays and return the length using the variables provided below. Expected output: 10
 
 const myNumbers1 = [3, 7, 0, 36, -9]
 const myNumbers2 = [8, -7, 42, 9, 13]
 
+// for my assessment i will use built in methods for these two arrays instead of building a function 
+    //In order to see the two arrays in one we will assign a var named bothArrays to the following step 
+    //the built in method .concat() allows us to combine two arrays         
+        //Once they are merged we can use the other built in method .length to count the amount of indexs in the array
+        //We will console.log() bothArrays.length
+
+        var bothArrays = myNumbers1.concat(myNumbers2)
+        console.log(bothArrays.length)
+
+        //Output ---> 10 
 
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "2202 eilrahC"
 
 const currentCohort = "Charlie 2022"
 
+   
 
+
+    
+
+
+
+   
 // --------------------4) Create a statement that will evaluate whether each number in an array is even or odd. Use the test variable provided below. Expected output: "odd" "even" "odd" "even" "odd" "even"
 
 const myArray = [13, 34, 5, 10, 27, 42]
+
+    // In order to go through each index of the array i created a for loop
+        // I iterated the loop by having i=0, having it run till i has gone through every index of myArray, and i++ which allows it to keep advancing 
+    // we have the means of getting through the array but we decide if its even or odd through a decision tree or an if else statement
+        // if i % which means remainder of whatever its divided by in this case 2 === 0
+            //if i divided by 2 has no remaineder then do whatever is in the code block if not move on to else 
+        // once the if statement comes back true it will log even if the if statement comes out false it will log odd
+
+for(let i=0; i < myArray.length; i++){
+    if(i % 2 === 0){
+        console.log('even')
+    } else {
+        console.log('odd')
+    }
+}
+        //Output ----> odd even odd even odd even
+
 
 
 // --------------------5) Create a function that takes in a set of any two numbers and subtracts the smaller number from the larger number using the sets of test variables provided below. Expected output: 42, 3
@@ -47,3 +111,26 @@ const number2 = 100
 // Set two:
 const number3 = 27
 const number4 = 24
+
+    //Create an arrow syntax function that takes in two arguments in this case two different numbers that i labeled num1, num2
+        //we need a conditional statement that decides which of the two numbers is greater and subtracts the smaller one from the bigger one 
+    //I compared if num1 > num2 if that boolean statement is true 
+        //returns num1 - num2 because num1 was greater 
+    //If it turns out to be false the num2 - num1 which means num2 was greater 
+
+const subtraction = (num1, num2) => {
+    if (num1 > num2){
+        return num1 - num2
+    } else {
+        return num2 - num1
+    }
+}
+
+console.log(subtraction(number1, number2))
+console.log(subtraction(number3, number4))
+console.log(subtraction(1937, 9723))
+
+    //Output ---> 42
+    //Output ---> 3
+    //Output ---> and a random number trial gave the result of 7786 which verified by a phone calculator was true 
+
--- coding-concepts.js
+++ coding-concepts.js
@@ -18,8 +18,8 @@ const colors = ["tangerine", "magenta", "lilac", "daffodil"]
 const cohort = "LEARN 2022"
 // console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: undefined because .length() is a built in method that can only be used on arrays
+// b) Verify and explain: I was wrong the .length without () can be used on a string but the .length() with quotes can only be used on an array
 
 
 // --------------------2) What will this log?
@@ -27,8 +27,8 @@ const cohort = "LEARN 2022"
 const greeting = "Hello World!"
 // console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: it will log index of 4 which is o
+// b) Verify and explain: I was right when a number is in brackets that butt up against a string or array it will log that index 
 
 
 // --------------------3) What will this log?
@@ -37,8 +37,8 @@ const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
 // console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: it should log ruby 
+// b) Verify and explain: I was right the variable index is assigned the number one there for it will take the first index which is ruby. Another thing to mention is that when counting places in an array or string the very first index is zero thats why it was not JavaScript. 
 
 
 // --------------------4) What will this log?
@@ -46,14 +46,14 @@ const index = 1
 const weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: it should uppercase every letter in the array Output ---> SATURDAY SUNDAY
+// b) Verify and explain: I was wrong the answer was undefined. The .toUpperCase() only works with strings. Because this is an array it would not work if you wanted to capatilize each letter you would have to convert the weekendDays array to a string and the .toUpperCase()
 
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean"]
-// console.log(typeof dataTypes.length)
+console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: it should log the word number 
+// b) Verify and explain: I was correct. It logs the word numbers because in the console.log() its asking the typeof the outcome of dataTypes.length. Which .length returns a number in this case 3. The typeof was then going off of the number 3 which logs to the console the word number.  
